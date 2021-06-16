fa3025e6def24e04b5ab12c30a179c78846aa96f
Guven Kurt <guven89@hotmail.com>
2021-06-16 08:12:35 +0200

    add update db endpoint and adjust SA password and expose port for sql server

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models;
+
+namespace PaymentGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly AppDbContext _appDbContext;
+
+        public AuthController(IConfiguration configuration, AppDbContext appDbContext)
+        {
+            _configuration = configuration;
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet(Name = nameof(GetToken))]
+        public ActionResult<string> GetToken()
+        {
+            var config = _configuration.GetSection(Constants.JwtKey).Get<JwtConfig>();
+
+            string token = Auth.GenerateJwtToken(config, Constants.TestUser);
+
+            return Ok(token);
+        }
+    }
+}
---
+++ API/Controllers/DatabaseController.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DatabaseController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet(Name = nameof(UpdateDatabase))]
+        public async Task<ActionResult> UpdateDatabase()
+        {
+            await _appDbContext.Database.MigrateAsync();
+
+            return Ok();
+        }
+    }
+}
